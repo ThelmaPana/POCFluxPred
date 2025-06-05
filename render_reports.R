7e8c6fa77494c1cc4f905c5a2603074c0794713c
ThelmaPana <thelma@crans.org>
2025-06-05 08:08:57 +0100

    Update options for individual report rendering

--- render_reports.R
+++ render_reports.R
@@ -10,39 +10,57 @@ library(glue)
 library(optparse)
 library(yaml)
 
+
+## Options ----
+#--------------------------------------------------------------------------#
 # Define options
 option_list <- list(
-  make_option(c("--refresh"), type = "character", default = NULL,
-              help = "Comma-separated list of experiment IDs to force refresh (e.g., 'exp_01,exp_03')"),
+  make_option(c("--include"), type = "character", default = NULL,
+              help = "Comma-separated list of experiment IDs to render reports for (e.g., 'exp_01,exp_03')"),
+  make_option(c("--refresh"), action = "store_true", default = FALSE,
+              help = "Refresh Quarto cache"),
   make_option(c("--no-cache"), dest = "no_cache", action = "store_true", default = FALSE,
               help = "Disable Quarto cache")
 )
 
-opt_parser <- OptionParser(option_list = option_list)
-opts <- parse_args(opt_parser)
+if (interactive()) {
+  # Simulate command line arguments when running interactively (in RStudio or R console)
+  command_args <- c(
+    #"--no-cache",
+    "--include=exp_04,exp_05",
+    "--refresh"
+  )
+} else {
+  # When running from terminal, take the real command line arguments
+  command_args <- commandArgs(trailingOnly = TRUE)
+}
 
-# Parse refresh list
-refresh_list <- strsplit(opts$refresh, ",")[[1]] %||% character()
+# Then pass command_args to OptionParser
+opts <- parse_args(OptionParser(option_list = option_list), args = command_args)
+
+
+## List directories of experiments ----
+#--------------------------------------------------------------------------#
+# Parse list of experiments to include 
+include_list <- strsplit(opts$include, ",")[[1]] %||% character()
 
 # List directory of experiments, excluding the demo experiment
 experiment_dirs <- dir_ls("experiments", type = "directory", regexp = "^experiments/exp_[0-9]+$")
 
+# Keep only directories for which reports should be rendered
+filtered_experiment_dirs <- experiment_dirs[path_file(experiment_dirs) %in% include_list]
+
+
+## Render reports ----
+#--------------------------------------------------------------------------#
 # Loop over experiments and generate reports
-for (dir in experiment_dirs) {
+for (dir in filtered_experiment_dirs) {
   exp_name <- path_file(dir)
-  force_refresh <- exp_name %in% refresh_list
   
   cat("Generating report for", exp_name, "\n")
   
   # Copy quarto report to target directory
   file_copy("R/report_experiment.qmd", path(dir, "report_experiment.qmd"), overwrite = TRUE)  
-  
-  # Clean cache if forced refresh
-  if (force_refresh) {
-    cat("Clearing cache for", exp_name, "\n")
-    cache_path <- path(dir, "_quarto")
-    if (dir_exists(cache_path)) {dir_delete(cache_path)}
-  }
   
   # Build render command
   cmd <- glue(
@@ -51,7 +69,8 @@ for (dir in experiment_dirs) {
     "--execute ",
     "--execute-param config_path='config.yaml' ",
     "--output report.html ",
-    if (opts$no_cache) {"--no-cache"} else {""}
+    if (opts$no_cache) {"--no-cache"} else {""},
+    if (opts$refresh) {"--cache-refresh"} else {""}
   )
   
   # Move to target dir and render document
